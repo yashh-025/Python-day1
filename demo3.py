5733db1ed74a89f757c09984403220e4956aa71a
yashh-025 <yashayare62@gmail.com>
2026-09-10 15:19:27 +0530

    pr3

---
+++ demo3.py
@@ -0,0 +1,4 @@
+time_in_min = int(input("enter time in minutes: "))
+time_in_hours = time_in_min // 60
+time_in_minutes = time_in_min % 60
+print(time_in_hours, " hours and ", time_in_minutes, " minutes")    
